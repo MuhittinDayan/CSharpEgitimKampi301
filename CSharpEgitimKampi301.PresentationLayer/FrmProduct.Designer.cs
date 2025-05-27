f4826d53cf1ff7bb8bdb72feec2400b406d1be7c
Muhittin Dayan <mhtndyn.34@gmail.com>
2025-05-27 08:51:45 +0300

    CSharpEgitimKampi301

---
+++ CSharpEgitimKampi301.PresentationLayer/FrmProduct.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace CSharpEgitimKampi301.PresentationLayer
+{
+    partial class FrmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtnPassive = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.rbtnActive = new System.Windows.Forms.RadioButton();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtProductStock = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProductPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.btnList2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(151, 18);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.Size = new System.Drawing.Size(152, 22);
+            this.txtProductId.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Ürün ID : ";
+            this.label2.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Ürün Adı : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Kategori Durumu : ";
+            // 
+            // rbtnPassive
+            // 
+            this.rbtnPassive.AutoSize = true;
+            this.rbtnPassive.Location = new System.Drawing.Point(226, 342);
+            this.rbtnPassive.Name = "rbtnPassive";
+            this.rbtnPassive.Size = new System.Drawing.Size(58, 20);
+            this.rbtnPassive.TabIndex = 22;
+            this.rbtnPassive.TabStop = true;
+            this.rbtnPassive.Text = "Pasif";
+            this.rbtnPassive.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(151, 423);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 34);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(151, 463);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(152, 34);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(151, 503);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(152, 34);
+            this.btnUpdate.TabIndex = 19;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.Location = new System.Drawing.Point(151, 543);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(152, 34);
+            this.btnGetById.TabIndex = 18;
+            this.btnGetById.Text = "ID\'ye Göre Getir";
+            this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(151, 383);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(69, 34);
+            this.btnList.TabIndex = 17;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // rbtnActive
+            // 
+            this.rbtnActive.AutoSize = true;
+            this.rbtnActive.Location = new System.Drawing.Point(151, 342);
+            this.rbtnActive.Name = "rbtnActive";
+            this.rbtnActive.Size = new System.Drawing.Size(53, 20);
+            this.rbtnActive.TabIndex = 16;
+            this.rbtnActive.TabStop = true;
+            this.rbtnActive.Text = "Aktif";
+            this.rbtnActive.UseVisualStyleBackColor = true;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(151, 59);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(152, 22);
+            this.txtProductName.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Gray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(343, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(886, 556);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Ürün Stok : ";
+            // 
+            // txtProductStock
+            // 
+            this.txtProductStock.Location = new System.Drawing.Point(151, 106);
+            this.txtProductStock.Name = "txtProductStock";
+            this.txtProductStock.Size = new System.Drawing.Size(152, 22);
+            this.txtProductStock.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 16);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Ürün Fiyatı :  ";
+            // 
+            // txtProductPrice
+            // 
+            this.txtProductPrice.Location = new System.Drawing.Point(151, 147);
+            this.txtProductPrice.Name = "txtProductPrice";
+            this.txtProductPrice.Size = new System.Drawing.Size(152, 22);
+            this.txtProductPrice.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 16);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Ürün Açıklama : ";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(151, 245);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(152, 87);
+            this.txtDescription.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 16);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "Kategori : ";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(151, 196);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(152, 24);
+            this.cmbCategory.TabIndex = 35;
+            // 
+            // btnList2
+            // 
+            this.btnList2.Location = new System.Drawing.Point(226, 383);
+            this.btnList2.Name = "btnList2";
+            this.btnList2.Size = new System.Drawing.Size(69, 34);
+            this.btnList2.TabIndex = 36;
+            this.btnList2.Text = "Listele2";
+            this.btnList2.UseVisualStyleBackColor = true;
+            this.btnList2.Click += new System.EventHandler(this.btnList2_Click);
+            // 
+            // FrmProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1267, 651);
+            this.Controls.Add(this.btnList2);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtProductPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtProductStock);
+            this.Controls.Add(this.txtProductId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbtnPassive);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.rbtnActive);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmProduct";
+            this.Text = "FrmProduct";
+            this.Load += new System.EventHandler(this.FrmProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProductId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnPassive;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.RadioButton rbtnActive;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtProductStock;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProductPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Button btnList2;
+    }
+}
---
+++ CSharpEgitimKampi301.PresentationLayer/FrmProduct.cs
@@ -0,0 +1,99 @@
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using CSharpEgitimKampi301.EntitiyLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.PresentationLayer
+{
+    
+    public partial class FrmProduct : Form
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        public FrmProduct()
+        {
+            InitializeComponent();
+            _productService = new ProductManager(new EFProductDal());
+            _categoryService = new CategoryManager(new EFCategoryDal());
+        }
+        // 1. adım yeni nesne üretmek
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            var values = _categoryService.TGetAll();
+            cmbCategory.DataSource = values; // veritabanından gelen verileri comboboxa atıyoruz
+            cmbCategory.DisplayMember = "CategoryName"; // comboboxda görünen isim
+            cmbCategory.ValueMember = "CategoryId"; // veritabanındaki karşılık gelen değer
+            // comboboxa veri atıyoruz
+        }
+
+
+        //2. adım listele butonunun görevini yazmak
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = _productService.TGetAll(); // tüm verileri çektik veritabanından
+            dataGridView1.DataSource = values; // verileri ekranda listeleme
+
+        }
+
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            var values =_productService.TGetProductsWithCatgory();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            _productService.TDelete(value);
+            MessageBox.Show("Silme işlemi başarılı");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.CategoryId = int.Parse(cmbCategory.SelectedValue.ToString());
+            product.ProductPrice = (int)decimal.Parse(txtProductPrice.Text);
+            product.ProductName = txtProductName.Text;
+            product.ProductStock = int.Parse(txtProductStock.Text);
+            product.ProductDescription = txtDescription.Text;
+            _productService.TInsert(product);
+            MessageBox.Show("Ekleme işlemi yapıldı");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            dataGridView1.DataSource = value;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductId.Text);
+            var value = _productService.TGetById(id);
+            value.ProductId = int.Parse(cmbCategory.SelectedItem.ToString());
+            value.ProductDescription = txtDescription.Text;
+            value.ProductName = txtProductName.Text;
+            value.ProductPrice = (int)decimal.Parse(txtProductPrice.Text);
+            value.ProductStock = int.Parse(txtProductStock.Text);
+            _productService.TUpdate(value);
+            MessageBox.Show("Güncelleme işlemi başarılı");
+
+        }
+    }
+}
